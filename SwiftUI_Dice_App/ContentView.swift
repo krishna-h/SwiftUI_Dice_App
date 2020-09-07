f9f25f0c4cdd9d4dfd864624d177977ff4c6cce4
krishna-h <67906241+krishna-h@users.noreply.github.com>
2020-09-07 11:00:10 -0700

    creating dice app

---
+++ SwiftUI_Dice_App/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  SwiftUI_Dice_App
+//
+//  Created by Mac on 07/09/20.
+//  Copyright © 2020 Gunde Ramakrishna Goud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
+    
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            VStack{
+            Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+struct DiceView: View {
+     let n : Int
+    var body: some View{
+        Image("dice\(n)")
+        .resizable()
+            .aspectRatio(1, contentMode: .fit)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
